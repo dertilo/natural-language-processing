8c345e7927c51287b838cf3835747fa88c584578
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2020-03-28 19:47:06 +0100

    refactored + ConvNNClassifier

--- torchtext_classification/run_text_clf.py
+++ torchtext_classification/run_text_clf.py
@@ -6,36 +6,32 @@ import torch
 
 from torchtext.datasets import text_classification
 
-from text_clf_models import EmbeddingBagClfModel
+from text_clf_models import EmbeddingBagClfModel, ConvNNClassifier
 from torch.utils.data.dataset import random_split
 
 from train_util import train_and_valid, evaluate, TrainParams
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Train a text classification model on text classification datasets."
-    )
-    parser.add_argument(
-        "--dataset", default="AG_NEWS", choices=text_classification.DATASETS
-    )
-    parser.add_argument("--ngrams", type=int, default=2, help="ngrams (default=2)")
-    args = parser.parse_args()
-    num_ngrams = 2
 
+    num_ngrams = 2
     num_epochs = 5
     embed_dim = 32
-    data = ".data"
     split_ratio = 0.95
 
+    data = ".data"
     if not os.path.exists(data):
         os.mkdir(data)
 
-    train_dataset, test_dataset = text_classification.DATASETS[args.dataset](
+    dataset_Name = "AG_NEWS"
+    assert dataset_Name in text_classification.DATASETS
+    train_dataset, test_dataset = text_classification.DATASETS[dataset_Name](
         root=data, ngrams=num_ngrams
     )
-    model = EmbeddingBagClfModel(
-        len(train_dataset.get_vocab()), embed_dim, len(train_dataset.get_labels())
-    )
+    vocab_size = len(train_dataset.get_vocab())
+    num_class = len(train_dataset.get_labels())
+
+    model = EmbeddingBagClfModel(vocab_size, embed_dim, num_class)
+    # model = ConvNNClassifier(vocab_size, embed_dim, num_class,16)
 
     criterion = torch.nn.CrossEntropyLoss()
 
@@ -46,11 +42,3 @@ if __name__ == "__main__":
     params = TrainParams(4.0, 0.8)
     train_and_valid(model, criterion, sub_train_, sub_valid_, params)
     print("Test - Accuracy: {}".format(evaluate(model, test_dataset)))
-
-    if args.save_model_path:
-        print("Saving model to {}".format(args.save_model_path))
-        torch.save(model.to("cpu"), args.save_model_path)
-
-    if args.dictionary is not None:
-        print("Save vocab to {}".format(args.dictionary))
-        torch.save(train_dataset.get_vocab(), args.dictionary)
--- torchtext_classification/text_clf_models.py
+++ torchtext_classification/text_clf_models.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class EmbeddingBagClfModel(nn.Module):
@@ -16,3 +17,38 @@ class EmbeddingBagClfModel(nn.Module):
 
     def forward(self, text, offsets):
         return self.fc(self.embedding(text, offsets))
+
+
+class ConvNNClassifier(nn.Module):
+    def __init__(self, vocab_size, embed_dim, num_classes, num_channels):
+        super().__init__()
+
+        self.embedding = nn.EmbeddingBag(vocab_size, embed_dim, sparse=True)
+        self.convnet = nn.Sequential(
+            nn.Conv1d(in_channels=embed_dim, out_channels=num_channels, kernel_size=3),
+            nn.ELU(),
+            nn.Conv1d(
+                in_channels=num_channels,
+                out_channels=num_channels,
+                kernel_size=3,
+                stride=2,
+            ),
+            nn.ELU(),
+            nn.Conv1d(
+                in_channels=num_channels,
+                out_channels=num_channels,
+                kernel_size=3,
+                stride=2,
+            ),
+            nn.ELU(),
+            nn.Conv1d(
+                in_channels=num_channels, out_channels=num_channels, kernel_size=3
+            ),
+            nn.ELU(),
+        )
+        self.fc = nn.Linear(num_channels, num_classes)
+
+    def forward(self, text, offsets):
+        x = self.embedding(text, offsets)
+        features = self.convnet(x).squeeze(dim=2)
+        return self.fc(features)
