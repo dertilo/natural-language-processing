dcfb7d315f7767f23a4b2c3a7a3f78467c6a1111
Tilo Himmelsbach <dertilo@gmail.com>
2020-04-21 22:07:00 +0200

    some cleaning

--- lightning-text-classification/bert_classifier.py
+++ lightning-text-classification/bert_classifier.py
@@ -12,7 +12,10 @@ from transformers import AutoModel
 
 import pytorch_lightning as pl
 from bert_tokenizer import BERTTextEncoder
-from sentiment_data_processing import sentiment_analysis_dataset, prepare_sample
+from sentiment_data_processing import (
+    prepare_sample,
+    load_dataset,
+)
 from test_tube import HyperOptArgumentParser
 from torchnlp.encoders import LabelEncoder
 from torchnlp.utils import lengths_to_mask
@@ -20,22 +23,14 @@ from utils import mask_fill
 
 
 class BERTClassifier(pl.LightningModule):
-    """
-    Sample model to show how to use BERT to classify sentences.
-    
-    :param hparams: ArgumentParser containing the hyperparameters.
-    """
 
     def __init__(self, hparams) -> None:
         super(BERTClassifier, self).__init__()
         self.hparams = hparams
         self.batch_size = hparams.batch_size
 
-        # build model
         self.__build_model()
-
-        # Loss criterion initialization.
-        self.__build_loss()
+        self.criterion = nn.CrossEntropyLoss()
 
         if hparams.nr_frozen_epochs > 0:
             self.freeze_encoder()
@@ -44,27 +39,22 @@ class BERTClassifier(pl.LightningModule):
         self.nr_frozen_epochs = hparams.nr_frozen_epochs
 
     def __build_model(self) -> None:
-        """ Init BERT model + tokenizer + classification head."""
         self.bert = AutoModel.from_pretrained(
             self.hparams.encoder_model, output_hidden_states=True
         )
 
-        # set the number of features our encoder model will return...
         if self.hparams.encoder_model == "google/bert_uncased_L-2_H-128_A-2":
             self.encoder_features = 128
         else:
             self.encoder_features = 768
 
-        # Tokenizer
         self.tokenizer = BERTTextEncoder("bert-base-uncased")
 
-        # Label Encoder
         self.label_encoder = LabelEncoder(
             self.hparams.label_set.split(","), reserved_labels=[]
         )
         self.label_encoder.unknown_index = None
 
-        # Classification head
         self.classification_head = nn.Sequential(
             nn.Linear(self.encoder_features, self.encoder_features * 2),
             nn.Tanh(),
@@ -73,12 +63,8 @@ class BERTClassifier(pl.LightningModule):
             nn.Linear(self.encoder_features, self.label_encoder.vocab_size),
         )
 
-    def __build_loss(self):
-        """ Initializes the loss function/s. """
-        self._loss = nn.CrossEntropyLoss()
 
     def unfreeze_encoder(self) -> None:
-        """ un-freezes the encoder layer. """
         if self._frozen:
             log.info(f"\n-- Encoder model fine-tuning")
             for param in self.bert.parameters():
@@ -86,18 +72,11 @@ class BERTClassifier(pl.LightningModule):
             self._frozen = False
 
     def freeze_encoder(self) -> None:
-        """ freezes the encoder layer. """
         for param in self.bert.parameters():
             param.requires_grad = False
         self._frozen = True
 
     def predict(self, sample: dict) -> dict:
-        """ Predict function.
-        :param sample: dictionary with the text we want to classify.
-
-        Returns:
-            Dictionary with the input text and the predicted label.
-        """
         if self.training:
             self.eval()
 
@@ -129,10 +108,7 @@ class BERTClassifier(pl.LightningModule):
         # from the entire original batch
         mask = lengths_to_mask(lengths, device=tokens.device)
 
-        # Run BERT model.
         word_embeddings = self.bert(tokens, mask)[0]
-
-        # Average Pooling
         word_embeddings = mask_fill(
             0.0, tokens, word_embeddings, self.tokenizer.padding_index
         )
@@ -143,50 +119,24 @@ class BERTClassifier(pl.LightningModule):
         return {"logits": self.classification_head(sentemb)}
 
     def loss(self, predictions: dict, targets: dict) -> torch.tensor:
-        """
-        Computes Loss value according to a loss function.
-        :param predictions: model specific output. Must contain a key 'logits' with
-            a tensor [batch_size x 1] with model predictions
-        :param labels: Label values [batch_size]
-
-        Returns:
-            torch.tensor with loss value.
-        """
-        return self._loss(predictions["logits"], targets["labels"])
+        return self.criterion(predictions["logits"], targets["labels"])
 
     def training_step(self, batch: tuple, batch_nb: int, *args, **kwargs) -> dict:
-        """ 
-        Runs one training step. This usually consists in the forward function followed
-            by the loss function.
-        
-        :param batch: The output of your dataloader. 
-        :param batch_nb: Integer displaying which batch this is
-
-        Returns:
-            - dictionary containing the loss and the metrics to be added to the lightning logger.
-        """
         inputs, targets = batch
         model_out = self.forward(**inputs)
         loss_val = self.loss(model_out, targets)
 
         # in DP mode (default) make sure if result is scalar, there's another dim in the beginning
-        if self.trainer.use_dp or self.trainer.use_ddp2:
+        if self.trainer.use_dp or self.trainer.use_ddp2:#TODO(tilo)
             loss_val = loss_val.unsqueeze(0)
 
         tqdm_dict = {"train_loss": loss_val}
         output = OrderedDict(
             {"loss": loss_val, "progress_bar": tqdm_dict, "log": tqdm_dict}
         )
-
-        # can also return just a scalar instead of a dict (return loss_val)
         return output
 
     def validation_step(self, batch: tuple, batch_nb: int, *args, **kwargs) -> dict:
-        """ Similar to the training step but with the model in eval mode.
-
-        Returns:
-            - dictionary passed to the validation_end function.
-        """
         inputs, targets = batch
         model_out = self.forward(**inputs)
         loss_val = self.loss(model_out, targets)
@@ -194,13 +144,12 @@ class BERTClassifier(pl.LightningModule):
         y = targets["labels"]
         y_hat = model_out["logits"]
 
-        # acc
         labels_hat = torch.argmax(y_hat, dim=1)
         val_acc = torch.sum(y == labels_hat).item() / (len(y) * 1.0)
         val_acc = torch.tensor(val_acc)
 
         if self.on_gpu:
-            val_acc = val_acc.cuda(loss_val.device.index)
+            val_acc = val_acc.cuda(loss_val.device.index)#TODO(tilo): why?
 
         # in DP mode (default) make sure if result is scalar, there's another dim in the beginning
         if self.trainer.use_dp or self.trainer.use_ddp2:
@@ -209,7 +158,6 @@ class BERTClassifier(pl.LightningModule):
 
         output = OrderedDict({"val_loss": loss_val, "val_acc": val_acc,})
 
-        # can also return just a scalar instead of a dict (return loss_val)
         return output
 
     def validation_end(self, outputs: list) -> dict:
@@ -221,7 +169,7 @@ class BERTClassifier(pl.LightningModule):
         """
         val_loss_mean = 0
         val_acc_mean = 0
-        for output in outputs:
+        for output in outputs: #TODO(tilo): this is shitty!
             val_loss = output["val_loss"]
 
             # reduce manually when using dp
@@ -259,15 +207,10 @@ class BERTClassifier(pl.LightningModule):
         return [optimizer], []
 
     def on_epoch_end(self):
-        """ Pytorch lightning hook """
         if self.current_epoch + 1 >= self.nr_frozen_epochs:
             self.unfreeze_encoder()
 
-    def __retrieve_dataset(self, train=True, val=True, test=True):
-        """ Retrieves task specific dataset """
-        return sentiment_analysis_dataset(self.hparams, train, val, test)
-
-    def _build_dataloader(self,dataset,do_shuffle=False):
+    def _build_dataloader(self, dataset, do_shuffle=False):
         return DataLoader(
             dataset=dataset,
             shuffle=do_shuffle,
@@ -281,13 +224,10 @@ class BERTClassifier(pl.LightningModule):
         )
 
     def train_dataloader(self) -> DataLoader:
-        dataset = self.__retrieve_dataset(val=False, test=False)[0]
-        return self._build_dataloader(dataset,True)
-
+        return self._build_dataloader(load_dataset(self.hparams.train_csv), True)
 
     def val_dataloader(self) -> DataLoader:
-        dataset = self.__retrieve_dataset(train=False, test=False)[0]
-        return self._build_dataloader(dataset)
+        return self._build_dataloader(load_dataset(self.hparams.train_csv))
 
     @classmethod
     def add_model_specific_args(
