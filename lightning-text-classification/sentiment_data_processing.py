31d0454f39820f9df3d7e08d63854404ea20bdd0
Tilo Himmelsbach <dertilo@gmail.com>
2020-04-21 21:51:58 +0200

    clean dataprocessing

--- lightning-text-classification/sentiment_data_processing.py
+++ lightning-text-classification/sentiment_data_processing.py
@@ -1,72 +1,26 @@
-# -*- coding: utf-8 -*-
 import pandas as pd
-
-from test_tube import HyperOptArgumentParser
 from torchnlp.datasets.dataset import Dataset
 from torchnlp.utils import collate_tensors
-from typing import Dict, List
 
 
-def collate_lists(text: list, label: list) -> List[Dict]:
-    """ Converts each line into a dictionary. """
-    collated_dataset = []
-    for i in range(len(text)):
-        collated_dataset.append({"text": str(text[i]), "label": str(label[i])})
-    return collated_dataset
+def load_dataset(path):
+    df = pd.read_csv(path)
+    text = list(df.text)
+    label = list(df.sentiment)
+    assert len(text) == len(label)
+    return Dataset([{"text": str(t), "label": str(l)} for t, l in zip(text, label)])
 
 
-def sentiment_analysis_dataset(
-    hparams: HyperOptArgumentParser, train=True, val=True, test=True
-):
-    """
-    Loads the Dataset from the csv files passed to the parser.
-    :param hparams: HyperOptArgumentParser obj containg the path to the data files.
-    :param train: flag to return the train set.
-    :param val: flag to return the validation set.
-    :param test: flag to return the test set.
-
-    Returns:
-        - Training Dataset, Development Dataset, Testing Dataset
-    """
-
-    def load_dataset(path):
-        df = pd.read_csv(path)
-        text = list(df.text)
-        label = list(df.sentiment)
-        assert len(text) == len(label)
-        return Dataset(collate_lists(text, label))
-
-    func_out = []
-    if train:
-        func_out.append(load_dataset(hparams.train_csv))
-    if val:
-        func_out.append(load_dataset(hparams.dev_csv))
-    if test:
-        func_out.append(load_dataset(hparams.test_csv))
-
-    return tuple(func_out)
-
-
-def prepare_sample(sample: list,tokenizer, label_encoder,prepare_target: bool = True) -> (dict, dict):
-    """
-    Function that prepares a sample to input the model.
-    :param sample: list of dictionaries.
-
-    Returns:
-        - dictionary with the expected model inputs.
-        - dictionary with the expected target labels.
-    """
+def prepare_sample(
+    sample: list, tokenizer, label_encoder, prepare_target: bool = True
+) -> (dict, dict):
     sample = collate_tensors(sample)
     tokens, lengths = tokenizer.batch_encode(sample["text"])
 
     inputs = {"tokens": tokens, "lengths": lengths}
 
     if not prepare_target:
-        return inputs, {}
-
-    # Prepare target:
-    try:
+        targets = {}
+    else:
         targets = {"labels": label_encoder.batch_encode(sample["label"])}
-        return inputs, targets
-    except RuntimeError:
-        raise Exception("Label encoder found an unknown label.")
+    return inputs, targets
