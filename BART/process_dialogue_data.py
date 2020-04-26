63f4e4c0c3b3cf9b8c393b4764eb93bb87a66231
Tilo Himmelsbach <dertilo@gmail.com>
2020-04-26 18:01:42 +0200

    refactor process_dialogue_data.py

--- BART/process_dialogue_data.py
+++ BART/process_dialogue_data.py
@@ -1,24 +1,31 @@
 import os
 import random
 
+from tqdm import tqdm
 from transformers import BartTokenizer
 from typing import List
 from util import data_io
 
 
-def generate_input_output(data,SEP_TOKEN, hist_len=3):
+def generate_coqa_seq2seq(file_name, SEP_TOKEN, hist_len=3):
+
+    data = data_io.read_json(os.environ["HOME"] + "/data/QA/coqa/" + file_name)["data"]
+
     def get_history(l: List, k, hist_len):
         return [d["input_text"] for d in l[max(0, k - hist_len) : (k + 1)]]
 
     for datum in data:
-        for k in range(0, len(datum["questions"])):
+        dialogue_len = len(datum["questions"])
+        for k in range(dialogue_len):
             q_hist = get_history(datum["questions"], k, hist_len)
             a_hist = get_history(datum["answers"], k, hist_len)
-            dialogue, target = build_input_target(datum["story"], q_hist, a_hist,SEP_TOKEN)
+            dialogue, target = build_input_target(
+                datum["story"], q_hist, a_hist, SEP_TOKEN
+            )
             yield dialogue, target
 
 
-def build_input_target(background, q_hist, a_hist,SEP_TOKEN):
+def build_input_target(background, q_hist, a_hist, SEP_TOKEN):
     def process(s):
         return s.replace("\n", "")
 
@@ -32,16 +39,17 @@ if __name__ == "__main__":
     tokenizer = BartTokenizer.from_pretrained("bart-large")
     # BOS = tokenizer.special_tokens_map['bos_token']
     SEP = tokenizer.special_tokens_map["sep_token"]
-    for file_name, ds in [
-        ("coqa-train-v1.0.json", "train"),
-        ("coqa-dev-v1.0.json", "val"),
-    ]:
-        data = data_io.read_json(os.environ["HOME"] + "/data/QA/coqa/" + file_name)[
-            "data"
-        ]
-        dialogues = list(generate_input_output(data,SEP))
-        random.shuffle(dialogues)
-        folder = "coqa_seq2eq"
-        os.makedirs(folder, exist_ok=True)
-        data_io.write_lines(folder + "/" + ds + ".source", (x for x, _ in dialogues))
-        data_io.write_lines(folder + "/" + ds + ".target", (x for _, x in dialogues))
+    datagenerators = {
+        "train": [generate_coqa_seq2seq("coqa-train-v1.0.json", SEP)],
+        "val": [generate_coqa_seq2seq("coqa-dev-v1.0.json", SEP)],
+    }
+    data_path = os.environ["HOME"] + "/data/seq2seq_dialogue"
+    os.makedirs(data_path, exist_ok=True)
+
+    for ds, gs in datagenerators.items():
+        with open(data_path + "/" + ds + ".source", mode="w") as s, open(
+            data_path + "/" + ds + ".target", mode="w"
+        ) as t:
+            for x, y in tqdm(((x, y) for g in gs for x, y in g)):
+                s.write(x + "\n")
+                t.write(y + "\n")
